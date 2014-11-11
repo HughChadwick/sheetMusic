0c4ea0bad18036054cdaa4f284e7aa010d483e52
HughChadwick <hugh.chadwick@gmail.com>
2014-11-11 17:29:29 +0000

    implement root controller

--- app/AppKernel.php
+++ app/AppKernel.php
@@ -17,6 +17,7 @@ class AppKernel extends Kernel
             new Doctrine\Bundle\DoctrineBundle\DoctrineBundle(),
             new Sensio\Bundle\FrameworkExtraBundle\SensioFrameworkExtraBundle(),
             new Jamax\SheetMusic\AbcBundle\AbcBundle(),
+            new Jamax\SheetMusic\AppBundle\AppBundle(),
         );
 
         if (in_array($this->getEnvironment(), array('dev', 'test'))) {
---
+++ src/Jamax/SheetMusic/AppBundle/AppBundle.php
@@ -0,0 +1,9 @@
+<?php
+
+namespace Jamax\SheetMusic\AppBundle;
+
+use Symfony\Component\HttpKernel\Bundle\Bundle;
+
+class AppBundle extends Bundle
+{
+}
